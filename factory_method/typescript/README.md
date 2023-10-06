284a89fc9e2bcb567af9841e1bb12f078d37f03d
fatich <fatih@itsmoneo.com>
2023-10-06 22:26:55 +0300

    added ts code for factory method

---
+++ factory_method/typescript/README.md
@@ -0,0 +1,16 @@
+# Factory Method in TypeScript
+
+Kullanım örnekleri: Fabrika Yöntemi modeli TypeScript kodunda yaygın olarak kullanılır. Kodunuz için yüksek düzeyde esneklik sağlamanız gerektiğinde çok kullanışlıdır.
+
+Tanımlama: Fabrika yöntemleri, sınıflardan nesneler oluşturan oluşturma yöntemleriyle tanınabilir. Nesne oluşturma sırasında sınıflar kullanılırken, fabrika yöntemlerinin dönüş türü genellikle soyut sınıf (abstract class) veya arayüz (interface) olarak bildirilir.
+
+## Kavramsal Örnek
+
+Bu örnek, Fabrika Yöntemi tasarım modelinin yapısını gösterir ve aşağıdaki sorulara odaklanır:
+
+- Hangi sınıflardan oluşuyor?
+
+- Bu sınıflar hangi rolleri oynuyor?
+
+- Desenin öğeleri ne şekilde ilişkilidir?
+
---
+++ factory_method/typescript/index.js
@@ -0,0 +1,107 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * The Creator class declares the factory method that is supposed to return an
+ * object of a Product class. The Creator's subclasses usually provide the
+ * implementation of this method.
+ */
+var Creator = /** @class */ (function () {
+    function Creator() {
+    }
+    /**
+     * Also note that, despite its name, the Creator's primary responsibility is
+     * not creating products. Usually, it contains some core business logic that
+     * relies on Product objects, returned by the factory method. Subclasses can
+     * indirectly change that business logic by overriding the factory method
+     * and returning a different type of product from it.
+     */
+    Creator.prototype.someOperation = function () {
+        // Call the factory method to create a Product object.
+        var product = this.factoryMethod();
+        // Now, use the product.
+        return "Creator: The same creator's code has just worked with ".concat(product.operation());
+    };
+    return Creator;
+}());
+/**
+ * Concrete Creators override the factory method in order to change the
+ * resulting product's type.
+ */
+var ConcreteCreator1 = /** @class */ (function (_super) {
+    __extends(ConcreteCreator1, _super);
+    function ConcreteCreator1() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    /**
+     * Note that the signature of the method still uses the abstract product
+     * type, even though the concrete product is actually returned from the
+     * method. This way the Creator can stay independent of concrete product
+     * classes.
+     */
+    ConcreteCreator1.prototype.factoryMethod = function () {
+        return new ConcreteProduct1();
+    };
+    return ConcreteCreator1;
+}(Creator));
+var ConcreteCreator2 = /** @class */ (function (_super) {
+    __extends(ConcreteCreator2, _super);
+    function ConcreteCreator2() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ConcreteCreator2.prototype.factoryMethod = function () {
+        return new ConcreteProduct2();
+    };
+    return ConcreteCreator2;
+}(Creator));
+/**
+ * Concrete Products provide various implementations of the Product interface.
+ */
+var ConcreteProduct1 = /** @class */ (function () {
+    function ConcreteProduct1() {
+    }
+    ConcreteProduct1.prototype.operation = function () {
+        return '{Result of the ConcreteProduct1}';
+    };
+    return ConcreteProduct1;
+}());
+var ConcreteProduct2 = /** @class */ (function () {
+    function ConcreteProduct2() {
+    }
+    ConcreteProduct2.prototype.operation = function () {
+        return '{Result of the ConcreteProduct2}';
+    };
+    return ConcreteProduct2;
+}());
+/**
+ * The client code works with an instance of a concrete creator, albeit through
+ * its base interface. As long as the client keeps working with the creator via
+ * the base interface, you can pass it any creator's subclass.
+ */
+function clientCode(creator) {
+    // ...
+    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+    console.log(creator.someOperation());
+    // ...
+}
+/**
+ * The Application picks a creator's type depending on the configuration or
+ * environment.
+ */
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ConcreteCreator1());
+console.log('');
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ConcreteCreator2());
